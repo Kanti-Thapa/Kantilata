a806c52b5d82b4d27fd53b95401ef669d249a9a1
Kanti-Thapa <101084350+Kanti-Thapa@users.noreply.github.com>
2024-03-04 09:43:07 -0600

    Add files via upload

---
+++ bootcsmp.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 16:40:11 2024
+
+@author: kantilata
+"""
+
+print("Hello world!")
+print(7+8)
+print(2**8)
+print("Hello")
+15+5
+print('how are \n\tyou today?')
+print('how\'s it going')
+
+
+print('One, \'two\', \"three\",four,\\five\\\n\tonce,\'I\' caught a \n\t\t\fish //alive\\\\\'')
+
+x=5
+y=12
+
+z=7+7
+
+p=x+y
+print(p)
+
+phrase_1="The cat sat on the mat!"
+phrase_2= "And so did the dog!"
+
+phrase_3= phrase_1 +' '+ phrase_2
+print(phrase_3)
+
+help()
+
+radius=float(input('Please enter the circle radius:'))
+pi=3.1445
+area=pi * radius**2
+print(radius,'the area of circle is', area)
+
+#convert fahrenheit to celsius
+far=    float(input('Please enter the temperature in fahrenheit:'))
+cel= (far-32) *5/9
+print('far, The temperature in celcius is', cel)
+
+ #obtain the sum of two integers
+ num_1=int(input('first number:'))
+ num_2=int(input('second number:'))
+ print('The sum of ' + str(num_1) + 'and' + str(num_2) + 'is' + str(num_1+num_2))
+ 
+ 
+#obatin the product of two intergers
+num_x=int(input('Write the first num:'))
+num_y=int(input('Write the secind num:'))
+#print('The product of ', str(num_x),'and', str(num_y), 'is', str(num_x*num_y))
+product=num_x*num_y
+print('The product of',str(num_x), 'and', str(num_y), 'is', product)
+
+#last exercise
+pizza_sold=6
+total_slices=4*4
+total_person=4
+pizza_slices_each=4
+pizza_needed= total_pizza/total_person
+print('The minimum number of pizza needed', 'is', pizza_needed)
+
+my_string="Python"
+len(my_string)
+my_string[0]
+my_string[0:4]
+my_string[-1]
+my_string[-2]
+my_string.upper()
+my_string.lower()
+
+
+
+
+
+
+
+
+
+    if user_input==1:
+        print('Please print it two')
+    elif user_input==3:
+        print('Please print three')
+    
+    else user_input==6:
+        print('This not a number')
+        
+    
+user_input=input('Please enter the string between one and Five:>')
+user_input=user_input.lower()
+if user_input=='one':
+    print(1)
+elif user_input=='two':
+    print(2)
+elif user_input=='three':
+    print(3)
+else:
+ print('It is out of range')
+
+
+#Question 3
+user_input=int(input('Write the variable between 1-10:>'))
+secret_number==3
+if user_input isdigit():
+    user_input==secret_number:
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
